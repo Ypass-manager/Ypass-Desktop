43b5942d19ffdd08e11ab5167ad3787a13baafd1
Soutif Thomas <th.soutif@gmail.com>
2023-10-25 17:33:46 +0200

    Init the project with a main structur
    That structur include :
    Use of WPF
    Use of .NET 7
    Include entityframework, but not the code yet
    MVVM design for the architectur
    Warning : For now the Page does not work as intended, I need to work on this case to have a better architectur base

---
+++ ViewModels/ExempleViewModel.cs
@@ -0,0 +1,15 @@
+using YpassDesktop.Views;
+
+namespace YpassDesktop.ViewModels
+{
+    public class ExempleViewModel : BaseViewModel
+    {
+
+        private ExempleViewModel exempleViewModel;
+
+        public ExempleViewModel(ExempleViewModel viewModel)
+        {
+            this.exempleViewModel = viewModel;
+        }
+    }
+}
---
+++ Views/ExemplePage.xaml.cs
@@ -0,0 +1,25 @@
+using YpassDesktop.ViewModels;   
+using System.Windows.Controls;
+using System;
+
+
+
+namespace YpassDesktop.Views 
+{
+    /// <summary>
+    /// Interaction logic for ExemplePage.xaml
+    /// </summary>
+    public partial class ExemplePage : Page
+    {
+        public ExemplePage()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
