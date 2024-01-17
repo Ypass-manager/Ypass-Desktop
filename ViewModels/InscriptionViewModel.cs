5960dc9d6197530fbe8ea0e714abeeee079bc8cc
YoPaCha <yoannchaplain78@gmail.com>
2024-01-17 15:20:20 +0100

    Corrected PageViews and made ViewModels for them

--- ViewModels/InscriptionViewModel.cs
+++ ViewModels/InscriptionViewModel.cs
@@ -1,12 +1,9 @@
 ﻿using ReactiveUI;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Net.Mail;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
+using YpassDesktop.Service;
 
 namespace YpassDesktop.ViewModels;
 
@@ -14,8 +11,14 @@ public class InscriptionViewModel : BaseViewModel
 {
     public InscriptionViewModel()
     {
-        // Listen to changes of DatabaseName, Password and ConfirmPassword and update CanNavigateNext accordingly
+        // Listen to changes of DatabaseName, Password and update CanNavigateNext accordingly
+        this.WhenAnyValue(x => x.DatabaseName, x => x.Password)
+            .Subscribe(_ => UpdateCanNavigateNext());
 
+        var canNavNext = this.WhenAnyValue(x => x.CanNavigateNext);
+
+        NavigateNextCommand = ReactiveCommand.Create(NavigateNext, canNavNext);
+        GoBackCommand = ReactiveCommand.Create(GoBack);
     }
 
     private string? _databaseName;
@@ -47,5 +50,40 @@ public class InscriptionViewModel : BaseViewModel
         set { this.RaiseAndSetIfChanged(ref _confirmPassword, value); }
     }
 
-    // WIP
+    private bool _CanNavigateNext;
+
+    public bool CanNavigateNext
+    {
+        get { return _CanNavigateNext; }
+        protected set { this.RaiseAndSetIfChanged(ref _CanNavigateNext, value); }
+    }
+
+    // Update CanNavigateNext. Allow next page if E-Mail and Password are valid
+    private void UpdateCanNavigateNext()
+    {
+        CanNavigateNext =
+                !string.IsNullOrEmpty(_databaseName)
+            && !string.IsNullOrEmpty(_password);
+    }
+
+
+    public ICommand NavigateNextCommand { get; }
+    private void NavigateNext()
+    {
+
+        var parameterBuilder = new ParameterBuilder();
+        parameterBuilder.Add("email", DatabaseName);
+        parameterBuilder.Add("password", Password);
+
+        Service.NavigationService.NavigateTo(new ThirdPageViewModel(), parameterBuilder);
+
+    }
+
+    public ICommand GoBackCommand { get; }
+    private void GoBack()
+    {
+        Console.WriteLine("GO BACK TO HE PREVIOUS PAGE");
+        Service.NavigationService.GoBack();
+
+    }
 }
