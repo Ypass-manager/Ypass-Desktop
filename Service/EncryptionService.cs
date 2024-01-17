38c89bac2cadba9b76644356f61a8328e4372c79
Thomas Soutif <th.soutif@gmail.com>
2024-01-17 12:00:16 +0100

    Service Encryption
    Add the function to initialize the database with the salt critical etc, but database not available for now so should update this function soon
    
    Add the function to encrypt and decrypt a password with the static navigation service

---
+++ Service/EncryptionService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+using Tmds.DBus.Protocol;
+namespace YpassDesktop.Service
+{
+    public static class EncryptionService
+    {
+        
+        private static byte[]? SALT_CRITICAL_ENCRYPT;
+        private static byte[]? derivation_key_with_salt;
+        private static byte[]? IV;
+        public static void InitializeDatabaseWithMasterPassword(string master_password)
+        {
+            if (master_password == null)
+            {
+                throw new Exception("Master Password should be set");
+            }
+            
+            var PASSWORD = Encoding.UTF8.GetBytes(master_password);
+            
+            // Generate the SALT_DERIVED KEY
+            var salt_derived_key = Utils.HashTool.GenerateRandomSalt(); // TODO : WE STORE IT TO THE DATABASE AND GET IT LATER
+
+            //Let's derivate it to obtain the derivation key for AES (the key part)
+            derivation_key_with_salt = Utils.HashTool.DeriveKey(PASSWORD, salt_derived_key);
+
+            // We generate the unique IV for aes
+            IV = Utils.EncryptionTool.GenerateIV(); // ON STOCKE IV en base de données par la suite, et on le récupére
+
+            // Now we generate the CRITICAL SALT that will be encrypt with AES
+            var SALT_CRITICAL = Encoding.UTF8.GetString(Utils.HashTool.GenerateRandomSalt());
+           
+            SALT_CRITICAL_ENCRYPT = Utils.EncryptionTool.EncryptStringToBytes_Aes(SALT_CRITICAL, derivation_key_with_salt, IV);
+
+            // WE STORE IN THE DATABASE THE SALT_CRITICAL_ENCRYPT
+
+            // TODO
+            // ------------------------------------------------------------ //
+
+        }
+
+        public static string EncryptPassword(string password)
+        {
+            if (SALT_CRITICAL_ENCRYPT == null || derivation_key_with_salt == null || IV == null)
+            {
+                // Handle the case where any of the variables is null
+                // (e.g., throw an exception, log an error, etc.)
+                throw new ArgumentNullException("One or more required parameters are null");
+            }
+            var salt_critical_decrypt = DecryptSaltCritical();
+            var encryptPassword = Utils.EncryptionTool.EncryptStringToBytes_Aes(password, salt_critical_decrypt, IV);
+
+            return Convert.ToBase64String(encryptPassword);
+
+        }
+
+        public static string DecryptPassword(string encrypt_password)
+        {
+            var salt_critical_decrypt = DecryptSaltCritical();
+            var decryptPassword = Utils.EncryptionTool.DecryptStringFromBytes_Aes(Convert.FromBase64String(encrypt_password!), salt_critical_decrypt!, IV!);
+            return decryptPassword;
+        }
+
+
+        private static byte[] DecryptSaltCritical()
+        {
+            string decrypt_pass = Utils.EncryptionTool.DecryptStringFromBytes_Aes(SALT_CRITICAL_ENCRYPT!, derivation_key_with_salt!, IV!); // IV EST RECUPERER DE LA BASE DE DONNES AINSI QUE SALT CRITICAL ENCRYPT
+            return Encoding.UTF8.GetBytes(decrypt_pass);
+        }
+    }
+}
--- ViewModels/EncryptTestPageViewModel.cs
+++ ViewModels/EncryptTestPageViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Tmds.DBus.Protocol;
+using YpassDesktop.Service;
 
 namespace YpassDesktop.ViewModels
 {
@@ -13,56 +14,24 @@ namespace YpassDesktop.ViewModels
 
         public EncryptTestPageViewModel() {
 
-            // For this example, the Master Password is
-            var master_password = Encoding.UTF8.GetBytes("J'adore7!mon$dechaTBec1ause;it1sLakeThat");
-            // Generate the SALT_DERIVED KEY
-            var salt_derived_key = Utils.HashTool.GenerateRandomSalt(); // WE STORE IT TO THE DATABASE AND GET IT LATER
 
-            //Let's derivate it to obtain the derivation key for AES (the key part)
+            password = "LeMotdePasseDeToto";
 
-            var derivation_key_with_salt = Utils.HashTool.DeriveKey(master_password, salt_derived_key);
+            EncryptionService.InitializeDatabaseWithMasterPassword("J'adore7!mon$dechaTBec1ause;it1sLakeThat");
 
-            // Let's derivate this derivation_key_with_salt to obtain a the IV for aes
+            string _encryptPassword = EncryptionService.EncryptPassword(password);
+            string _decryptPassword = EncryptionService.DecryptPassword(_encryptPassword);
+            encryptPassword = _encryptPassword;
+            decryptPassword = _decryptPassword;
 
-            
-            byte[] IV = Utils.EncryptionTool.GenerateIV(); // ON STOCKE IV en base de données par la suite, et on le récupére
-      
-
-            // Now we generate the CRITICAL SALT that will be encrypt with AES
-
-            var SALT_CRITICAL = Encoding.UTF8.GetString(Utils.HashTool.GenerateRandomSalt());
-            Console.WriteLine("SALT CRITICAL BEFORE ENCRYPT : ", SALT_CRITICAL);
-
-            var SALT_CRITICAL_ENCRYPT = Utils.EncryptionTool.EncryptStringToBytes_Aes(SALT_CRITICAL, derivation_key_with_salt, IV);
-            Console.WriteLine("SALT CRITICAL ENCRYPT", SALT_CRITICAL_ENCRYPT);
-
-            // WE STORE IN THE DATABASE THE SALT_CRITICAL_ENCRYPT
-
-
-            // ------------------------------------------------------------ //
-
-            // Now let's encrypt a user password
-
-            // WE SHOULD FIRST GET THE SALT_CRITICAL
-
-            var salt_critical_decrypt = Utils.EncryptionTool.DecryptStringFromBytes_Aes(SALT_CRITICAL_ENCRYPT, derivation_key_with_salt, IV); // IV EST RECUPERER DE LA BASE DE DONNES AINSI QUE SALT CRITICAL ENCRYPT
-            Console.WriteLine("SALT CRITICAL AFTER DECRYPT", salt_critical_decrypt);
-            var encryptPassword = Utils.EncryptionTool.EncryptStringToBytes_Aes("LeMotdePasseDeToto", Encoding.UTF8.GetBytes(salt_critical_decrypt), IV);
-
-
-
-
-
-            // exposer our variable to the front
-            SaltCritical = SALT_CRITICAL;
-            SaltCriticalEncrypt = Convert.ToBase64String(SALT_CRITICAL_ENCRYPT);
-            SaltCriticalDecrypt = salt_critical_decrypt;
         }
 
 
-        public string SaltCritical { get; }
-        public string SaltCriticalEncrypt { get; }
-        public string SaltCriticalDecrypt { get; }
+        
+        public string password { get; }
+        public string encryptPassword { get; }
+        public string decryptPassword { get; }
+
 
     }
 }
