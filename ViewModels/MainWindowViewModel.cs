656f2eae770dae713a4220181341bf0143c47b17
Michel <michel.guy@ynov.com>
2024-03-06 15:07:25 +0100

    add page

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -12,7 +12,7 @@ public class MainWindowViewModel : BaseViewModel
     private BaseViewModel _CurrentPage;
     public MainWindowViewModel()
     {
-        var simplePageViewModel= new EncryptTestPageViewModel();
+        var simplePageViewModel= new SimplePageViewModel();
         NavigationService.Initialize(simplePageViewModel);
         
         //Subscribe to the service to know when a page has been change, and set the page
