a7b126a6f7c572321034d95f170640c5e8e36281
Thomas Landete <thomas.landete@ynov.com>
2024-03-20 11:51:20 +0100

    HistoriqueDeConnexion
    Première version de l'historique de connexion

--- Model/HistoryConnection.cs
+++ Model/HistoryConnection.cs
@@ -1,11 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tmds.DBus.Protocol;
+using YpassDesktop.DataAccess;
 
 public class HistoryConnection
 {
-    public int historyConnectionId { get; set; }
-    public DateTime connectionDate { get; set; }
+    private YpassDbContext _dbContext;
+    public HistoryConnection(YpassDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        Connections = new List<DateTime>();
+    }
+    private DateTime LastConnection { get; set; }
+    public List<DateTime> Connections { get; set; }
+
+
+
+    //Add Each connectionDate
+    public void UpdateHistory()
+    {
+        LastConnection = DateTime.Now;
+        Connections.Add(LastConnection);
+        _dbContext.SaveChanges();
+    }
+
+
+    //Foreach Connection in Connections
+    /*public void BrowseHistory()
+    {
+        foreach (DateTime Connection in Connections)
+        {
+            
+        }
+    }*/
+
+    
 }
