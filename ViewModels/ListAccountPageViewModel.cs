c028724165ddd9165473a99b27d5c5512953e521
Soutif Thomas <th.soutif@gmail.com>
2024-04-13 00:15:16 +0200

    rename AccountList to ListAccount

--- ViewModels/ListAccountPageViewModel.cs
+++ ViewModels/ListAccountPageViewModel.cs
@@ -1,21 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
 using ReactiveUI;
-using System.Reactive.Linq;
-using YpassDesktop.Service;
-using YpassDesktop.DataAccess;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Windows.Input;
+using YpassDesktop.DataAccess;
+using YpassDesktop.Service;
+using Avalonia.Collections;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using System.Collections.ObjectModel;
+using AccountObj = YpassDesktop.Class.Account;
+namespace YpassDesktop.ViewModels;
 
-namespace YpassDesktop.ViewModels
+public class ListAccountPageViewModel : BaseViewModel, INotifyPropertyChanged
 {
-    public class ListAccountPageViewModel : BaseViewModel
-    {
-        public ListAccountPageViewModel()
+    // For now, exists only to make HomePageView.axaml available for testing
+    // Will be worked on later
+    protected readonly YpassDbContext _dbContext;
+    private ObservableCollection<AccountObj> _accounts;
+
+    public ListAccountPageViewModel() {
+
+        _dbContext = new YpassDbContext("HomePageDB.db");
+        try
         {
+            EncryptionService.LoadDatabaseWithMasterPassword("mdp", "HomePageDB.db");
+        }
+        catch
+        {
+            EncryptionService.InitializeDatabaseWithMasterPassword("mdp", "HomePageDB.db");
         }
 
+        Accounts = new ObservableCollection<AccountObj>
+        {
+            new AccountObj { Email = "custom1@gmail.com", Password = "*******************" },
+            new AccountObj { Email = "custom2@gmail.com", Password = "*******************" },
+        };
+
+    }
+
+    public ObservableCollection<AccountObj> Accounts
+    {
+        get => _accounts;
+        set => this.RaiseAndSetIfChanged(ref _accounts, value);
     }
 }
