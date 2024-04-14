af9082212e290e0769cb9b316089b1a51efebd6b
Thomas Soutif <th.soutif@gmail.com>
2024-04-14 23:31:26 +0200

    now the show / hide button for password works

--- Class/Account.cs
+++ Class/Account.cs
@@ -1,20 +1,55 @@
+using ReactiveUI;
 using System;
 using System.Windows.Input;
+using YpassDesktop.ViewModels;
 
 namespace YpassDesktop.Class;
 
-public class Account
+public class Account : BaseViewModel
 {
     // For now, exists only to make HomePageView.axaml available for testing
     // Will be worked on later
+
+    private string _passwordEncrypt = string.Empty;
+    private bool _isPasswordVisible = false;
     public Account() {
 
+        TogglePasswordVisibilityCommand = ReactiveCommand.Create(tooglePasswordVisibility);
 
     }
     
-public string Username { get; set; }
-public string Password { get; set; }
-public string Title { get; set;}
+    public string Username { get; set; }
+    public string Password
+    {
+        get
+        {
+            if (IsPasswordVisible)
+                return _passwordEncrypt;
+            else
+                return new string('*', _passwordEncrypt.Length);
+        }
+        set => this.RaiseAndSetIfChanged(ref _passwordEncrypt, value);
+    }
+    public void TogglePasswordVisibility()
+    {
+        IsPasswordVisible = !IsPasswordVisible;
+    }
+    public string Title { get; set;}
+    public bool IsPasswordVisible
+    {
+        get { return _isPasswordVisible; }
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _isPasswordVisible, value);
+            this.RaisePropertyChanged(nameof(Password));
+        }
+    }
+    public ICommand TogglePasswordVisibilityCommand { get; }
+
+    private void tooglePasswordVisibility()
+    {
+        TogglePasswordVisibility();
+    }
 
 }
 
